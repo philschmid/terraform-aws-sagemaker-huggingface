511b23afc19b303e4c078c5c38d280f97c22f2a1
philschmid <schmidphilipp1995@gmail.com>
2022-02-07 13:22:18 +0100

    adjust examples

--- examples/deploy_from_s3/main.tf
+++ examples/deploy_from_s3/main.tf
@@ -1,13 +1,5 @@
-
-# Configure the AWS Provider
-provider "aws" {
-  region  = "us-east-1"
-  profile = "hf-sm"
-
-}
-
 # ---------------------------------------------------------------------------------------------------------------------
-# Example Deploy from HuggingFace Hub
+# Example Deploy from Amazon s3
 # ---------------------------------------------------------------------------------------------------------------------
 
 module "huggingface_sagemaker" {
@@ -17,6 +9,6 @@ module "huggingface_sagemaker" {
   transformers_version = "4.12.3"
   instance_type        = "ml.g4dn.xlarge"
   instance_count       = 1 # default is 1
-  model_data           = "s3://hf-sagemaker-inference/example_custom_inference/model.tar.gz"
+  model_data           = "s3://my-bucket/mypath/model.tar.gz"
   hf_task              = "text-classification"
 }
